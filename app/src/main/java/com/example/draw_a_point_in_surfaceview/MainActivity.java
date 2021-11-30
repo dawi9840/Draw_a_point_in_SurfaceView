31c5ec40faa8cc80fb127741b4fb57068fb0854b
Dawi Lu <dawi9840@gmail.com>
2021-11-30 17:41:05 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/draw_a_point_in_surfaceview/MainActivity.java
+++ app/src/main/java/com/example/draw_a_point_in_surfaceview/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.draw_a_point_in_surfaceview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.FloatEvaluator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -31,13 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         DrawLine drawLine = new DrawLine(this);
 //        drawLine.setBackgroundColor(Color.WHITE);
-
-
         drawLine.setBackgroundColor(Color.TRANSPARENT);
-
 
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -50,34 +47,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class DrawLine extends View {
-        Paint paint = new Paint();
-        Paint paint1 = new Paint();
-        Paint paint2 = new Paint();
+        Paint p_red = new Paint();
+        Paint p_blue = new Paint();
+        Paint p_green = new Paint();
         public DrawLine(Context context) {
             super(context);
-            paint.setColor(Color.RED);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(10);
-            paint.setAntiAlias(true);
+            p_red.setColor(Color.RED);
+            p_red.setStyle(Paint.Style.STROKE);
+            p_red.setStrokeWidth(10);
+            p_red.setAntiAlias(true);
 
-            paint1.setColor(Color.BLUE);
-            paint1.setStyle(Paint.Style.STROKE);
-            paint1.setStrokeWidth(10);
-            paint1.setAntiAlias(true);
+            p_blue.setColor(Color.BLUE);
+            p_blue.setStyle(Paint.Style.STROKE);
+            p_blue.setStrokeWidth(10);
+            p_blue.setAntiAlias(true);
 
-            paint2.setColor(Color.GREEN);
-            paint2.setStyle(Paint.Style.STROKE);
-            paint2.setStrokeWidth(10);
-            paint2.setAntiAlias(true);
+            p_green.setColor(Color.GREEN);
+            p_green.setStyle(Paint.Style.STROKE);
+            p_green.setStrokeWidth(10);
+            p_green.setAntiAlias(true);
         }
 
         @Override
         public void onDraw(Canvas canvas) {
             float cap_w = 1088;
-            float cap_h = 2280;
+            float cap_h = 1088;
             float specific_frame = 300;
 
-            DisplayMetrics displayMetrics = new DisplayMetrics();
+            @SuppressLint("DrawAllocation") DisplayMetrics displayMetrics = new DisplayMetrics();
             getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
             int screen_w = displayMetrics.widthPixels;
@@ -86,57 +83,73 @@ public class MainActivity extends AppCompatActivity {
 
             if(navigationBar == 1){
                 getNavigationBarHeight();
-                screen_h = displayMetrics.heightPixels + getNavigationBarHeight(); // 2280
+                screen_h = displayMetrics.heightPixels + getNavigationBarHeight();
             }else{
-                screen_h = displayMetrics.heightPixels; //2069
+                screen_h = displayMetrics.heightPixels;
             }
 
+            System.out.println("screen_w:" + screen_w + ", screen_h:" + screen_h); // 1440, 3040
 
-            canvas.drawLine(specific_frame, specific_frame, specific_frame, cap_h-specific_frame, paint2);
-            canvas.drawLine(cap_w-specific_frame, specific_frame, cap_w-specific_frame, cap_h-specific_frame, paint2);
-            canvas.drawLine(specific_frame, specific_frame, cap_w-specific_frame, specific_frame, paint2);
-            canvas.drawLine(specific_frame, cap_h-specific_frame, cap_w-specific_frame, cap_h-specific_frame, paint2);
-
-//            canvas.drawLine(specific_frame, specific_frame, specific_frame, screen_h-specific_frame, paint);
-//            canvas.drawLine(screen_w-specific_frame, specific_frame, screen_w-specific_frame, screen_h-specific_frame, paint);
-//            canvas.drawLine(specific_frame, specific_frame, screen_w-specific_frame, specific_frame, paint);
-//            canvas.drawLine(specific_frame, screen_h-specific_frame, screen_w-specific_frame, screen_h-specific_frame, paint);
-
-            float add_value = 50.0F;
+            // AiVirtualMouseProject define area.
+            canvas.drawRect(specific_frame, specific_frame, cap_w-specific_frame, cap_h-specific_frame, p_green);
 
             // pixel_p8:(234.228104, 1070.018188), convert_p8:(69182.609375, 1597255.125000)
-            List<Float> pixel_p8_L_DOWN = new ArrayList<>();
+            @SuppressLint("DrawAllocation") List<Float> pixel_p8_L_DOWN = new ArrayList<>();
             pixel_p8_L_DOWN.add((float) 234.228104);
             pixel_p8_L_DOWN.add((float) 1070.018188);
-            canvas.drawLine(pixel_p8_L_DOWN.get(0)-add_value, pixel_p8_L_DOWN.get(1), pixel_p8_L_DOWN.get(0)+add_value, pixel_p8_L_DOWN.get(1), paint1);
-            canvas.drawLine(pixel_p8_L_DOWN.get(0), pixel_p8_L_DOWN.get(1)-add_value, pixel_p8_L_DOWN.get(0), pixel_p8_L_DOWN.get(1)+add_value, paint1);
 
             // pixel_p8:(218.616226, 34.274849), convert_p8:(64623.937500, 51926.074219)
-            List<Float> pixel_p8_L_UP = new ArrayList<>();
+            @SuppressLint("DrawAllocation") List<Float> pixel_p8_L_UP = new ArrayList<>();
             pixel_p8_L_UP.add((float) 218.616226);
             pixel_p8_L_UP.add((float) 34.274849);
-            canvas.drawLine(pixel_p8_L_UP.get(0)-add_value, pixel_p8_L_UP.get(1), pixel_p8_L_UP.get(0)+add_value, pixel_p8_L_UP.get(1), paint1);
-            canvas.drawLine(pixel_p8_L_UP.get(0), pixel_p8_L_UP.get(1)-add_value, pixel_p8_L_UP.get(0), pixel_p8_L_UP.get(1)+add_value, paint1);
 
             // pixel_p8:(847.106262, 19.184063), convert_p8:(248143.031250, 29410.621094)
-            List<Float> pixel_p8_R_UP = new ArrayList<>();
+            @SuppressLint("DrawAllocation") List<Float> pixel_p8_R_UP = new ArrayList<>();
             pixel_p8_R_UP.add((float) 847.106262);
             pixel_p8_R_UP.add((float) 19.184063);
-            canvas.drawLine(pixel_p8_R_UP.get(0)-add_value, pixel_p8_R_UP.get(1), pixel_p8_R_UP.get(0)+add_value, pixel_p8_R_UP.get(1), paint1);
-            canvas.drawLine(pixel_p8_R_UP.get(0), pixel_p8_R_UP.get(1)-add_value, pixel_p8_R_UP.get(0), pixel_p8_R_UP.get(1)+add_value, paint1);
 
             // pixel_p8:(863.990051, 1075.021973), convert_p8:(253073.093750, 1604720.750000)
-            List<Float> pixel_p8_R_DOWN = new ArrayList<>();
+            @SuppressLint("DrawAllocation") List<Float> pixel_p8_R_DOWN = new ArrayList<>();
             pixel_p8_R_DOWN.add((float) 863.990051);
             pixel_p8_R_DOWN.add((float) 1075.021973);
-            canvas.drawLine(pixel_p8_R_DOWN.get(0)-add_value, pixel_p8_R_DOWN.get(1), pixel_p8_R_DOWN.get(0)+add_value, pixel_p8_R_DOWN.get(1), paint1);
-            canvas.drawLine(pixel_p8_R_DOWN.get(0), pixel_p8_R_DOWN.get(1)-add_value, pixel_p8_R_DOWN.get(0), pixel_p8_R_DOWN.get(1)+add_value, paint1);
 
             // Mapping area.
-            canvas.drawLine(pixel_p8_L_UP.get(0), pixel_p8_R_UP.get(1), pixel_p8_R_UP.get(0), pixel_p8_R_UP.get(1), paint);
-            canvas.drawLine(pixel_p8_L_UP.get(0), pixel_p8_R_DOWN.get(1), pixel_p8_R_UP.get(0), pixel_p8_R_DOWN.get(1), paint);
-            canvas.drawLine(pixel_p8_L_UP.get(0), pixel_p8_R_UP.get(1), pixel_p8_L_UP.get(0), pixel_p8_R_DOWN.get(1), paint);
-            canvas.drawLine(pixel_p8_R_UP.get(0), pixel_p8_R_UP.get(1), pixel_p8_R_UP.get(0), pixel_p8_R_DOWN.get(1), paint);
+            canvas.drawRect(pixel_p8_L_UP.get(0), pixel_p8_R_UP.get(1), pixel_p8_R_DOWN.get(0), pixel_p8_R_DOWN.get(1), p_blue);
+
+            canvas.drawPoint(pixel_p8_L_UP.get(0), pixel_p8_L_UP.get(1), p_red);
+            canvas.drawPoint(pixel_p8_L_DOWN.get(0), pixel_p8_L_DOWN.get(1), p_red);
+            canvas.drawPoint(pixel_p8_R_UP.get(0), pixel_p8_R_UP.get(1), p_red);
+            canvas.drawPoint(pixel_p8_R_DOWN.get(0), pixel_p8_R_DOWN.get(1), p_red);
+
+
+            canvas.drawPoint((float)214.279999, (float)-1.649035, p_red);  // close, L_UP
+            canvas.drawPoint((float)222.160675, (float) 5.394957, p_red); // far, L_UP
+
+            canvas.drawPoint((float) 851.655212, (float) 13.403265, p_red); // close, R_UP
+            canvas.drawPoint((float) 852.348267, (float) 18.792240, p_red); // far, R_UP
+
+            canvas.drawPoint((float) 220.247574, (float) 1083.393188, p_red); // close, L_DOWN
+            canvas.drawPoint((float) 223.997360, (float) 1081.489990, p_red); // far, L_DOWN
+
+            canvas.drawPoint((float) 857.866272, (float) 1084.606445, p_red); // close, R_DOWN
+            canvas.drawPoint((float) 860.963135, (float) 1083.215942, p_red); // far, R_DOWN
+
+            // Convert Coordinates
+            @SuppressLint("DrawAllocation") List<Float> convert_p8 = new ArrayList<>();
+            @SuppressLint("DrawAllocation") FloatEvaluator f = new FloatEvaluator();
+            convert_p8.add(
+                    f.evaluate(
+                            pixel_p8_R_DOWN.get(0), (cap_w - specific_frame), (screen_w)
+                    )
+            );
+            convert_p8.add(
+                    f.evaluate(
+                            pixel_p8_R_DOWN.get(1), (cap_h - specific_frame), (screen_h)
+                    )
+            );
+
+            canvas.drawPoint(convert_p8.get(0), convert_p8.get(1), p_green);
+
         }
 
         private int isHaveNavigationBar(){
@@ -160,9 +173,6 @@ public class MainActivity extends AppCompatActivity {
             else
                 return 0;
         }
-
     }
-
-
 
 }
